8da4f90497faa073b5dd12a5cf76858ae8f6bf66
Ismail Elshakankiry <ismailshak94@yahoo.com>
2019-06-25 11:26:54 -0400

    adds css to Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
+import {Link} from 'react-router-dom'
 import "./Home.css";
+import Popular from '../Popular'
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Rec-Creation!</h1>
-        <h4>Welcome to Rec-Creation!</h4>
-        <p>The place to look up your favorite games and events.</p>
-        <button type="button">Host</button>
-        <button type="button">Search</button>
+      <div className="Home">
+        <div className="home-info-container">
+          <div className="home-text-container">
+            <h1>Welcome to Rec|Creation!</h1>
+            <p>Looking for ideas? Want to host an event? <br />
+            Find it all here. Have fun and meet new people!
+            (proper marketing intro needed</p>
+          </div>
+        </div>
+        <div className="home-buttons-container">
+            <Link className="nav-buttons" to="/host">Host</Link>
+            <Link className="nav-buttons" to="/search">Search</Link>
+          </div>
+        
+        
+        <Popular />
       </div>
     );
   }
