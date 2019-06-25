841db0bebadcf0fe8c784e0c1028e3f481f27cc6
Sean Urbassik <sean.urbassik@gmail.COM>
2019-06-25 13:23:31 -0400

    started host css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import "./Home.css";
-import Popular from '../Popular'
+// import Popular from '../Popular'
 
 class Home extends Component {
   render() {
@@ -10,18 +10,23 @@ class Home extends Component {
         <div className="home-info-container">
           <div className="home-text-container">
             <h1>Welcome to Rec|Creation!</h1>
-            <p>Looking for ideas? Want to host an event? <br />
-            Find it all here. Have fun and meet new people!
-            (proper marketing intro needed</p>
+            <p>
+              Looking for ideas? Want to host an event? <br />
+              Find it all here. Have fun and meet new people! (proper marketing
+              intro needed
+            </p>
           </div>
         </div>
         <div className="home-buttons-container">
-            <Link className="nav-buttons" to="/host">Host</Link>
-            <Link className="nav-buttons" to="/search">Search</Link>
-          </div>
-        
-        
-        <Popular />
+          <Link className="nav-buttons" to="/host">
+            Host
+          </Link>
+          <Link className="nav-buttons" to="/search">
+            Search
+          </Link>
+        </div>
+
+        {/* <Popular /> */}
       </div>
     );
   }
--- src/components/Host/Host.js
+++ src/components/Host/Host.js
@@ -4,23 +4,30 @@ import "./Host.css";
 class Host extends Component {
   render() {
     return (
-      <div>
-        <h3>Host an Event! Fill out the form belwo.</h3>
-        <form>
-          <select className="Game">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-          </select>
-          <input type="text" name="location" />
-          <select className="Type">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-          </select>
-          <input type="text" name="participants" />
-          <input type="submit" value="Submit" />
-        </form>
+      <div className="Host">
+        <div className="host-info-container">
+          <form className="host-form">
+            <h3>Host an Event! Fill out the form below.</h3>
+            Game
+            <select className="Game">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+            </select>
+            Location
+            <input type="text" className="location" />
+            Type
+            <select className="Type">
+              <option>Indoor</option>
+              <option>Outdoor</option>
+            </select>
+            Number of Participants
+            <input type="text" className="participants" />
+            Discription
+            <textarea name="participants" />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
